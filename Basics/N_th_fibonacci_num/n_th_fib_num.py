1798dc6850aea1d22e0bd1c33c7792c656465b2a
Hasan Sabbir <rockeyhasan4@gmail.com>
2026-08-05 17:31:08 +0800

    Added the solution of n-th-fibonacci number.

---
+++ Basics/N_th_fibonacci_num/n_th_fib_num.py
@@ -0,0 +1,19 @@
+def get_fibb_num(n):
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    first = 0
+    second = 1
+
+    for _ in range(2, n + 1):
+        next = first + second
+        first = second
+        second = next
+
+    return second
+
+n = int(input("Enter the value of N: "))
+print(get_fibb_num(n))
