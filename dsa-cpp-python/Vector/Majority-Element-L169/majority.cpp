3f66db9666ef4687ffd882cd32ad910f82d39295
Hasan Sabbir <rockeyhasan4@gmail.com>
2026-09-10 16:07:26 +0800

    Better version for majority problem

--- dsa-cpp-python/Vector/Majority-Element-L169/majority.cpp
+++ dsa-cpp-python/Vector/Majority-Element-L169/majority.cpp
@@ -3,21 +3,34 @@
 using namespace std;
 
 int majority(vector<int> &list){
-    int initial = list[0]; int count = 0;
+    int candidate = list[0]; int count = 0;
     for(int el : list){
         if(count == 0){
-            initial = el; count = 1;
-        } else if(initial == el){
+            candidate = el; count = 1;
+        } else if(candidate == el){
             count++;
         } else{
             count--;
         }
     }
-    return initial;
+
+    count = 0;
+
+    for (int el : list) {
+        if (el == candidate) {
+            count++;
+        }
+    }
+
+    if(count > list.size()/2){
+        return candidate;
+    }
+
+    return -1;
 }
 
 int main(){
-    vector<int> list = {2,2,1,1,1,1};
+    vector<int> list = {1, 2, 5, 10, 15, 2, 2, 2, 2};
     cout<< majority(list);
 
     return 0;
